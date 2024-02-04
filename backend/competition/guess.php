70a83f6a82c89688758a6dba7653b3e5026dfb22
HSZemi <hszemi@users.noreply.github.com>
2024-02-04 22:37:14 +0100

    Add instructions to guess.php

--- backend/competition/guess.php
+++ backend/competition/guess.php
@@ -2,8 +2,9 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-echo '{"success":true, "message":"Sadly, voting has closed already. Have fun during the reveal!"}';
-die();
+// Remove the // from the next two lines to close the voting.
+//echo '{"success":true, "message":"Sadly, voting has closed already. Have fun during the reveal!"}';
+//die();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$filename = 'data/' . bin2hex(random_bytes(16)) . '-' . time() . '.json';
 	if(FALSE === file_put_contents($filename, file_get_contents('php://input'))){
@@ -12,5 +13,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		echo '{"success":true, "message":"Your guess has been saved successfully! You can close this window now."}';
 	}
 }
-//$data = json_decode(file_get_contents('php://input'), true);
-//file_put_contents('data/guess.json', json_encode($data, JSON_PRETTY_PRINT));
